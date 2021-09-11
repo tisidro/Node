69b631c38a23922a0c27ab506ba5098985993f7f
Terri Isidro <terri.isidro@gmail.com>
2021-09-11 15:07:36 -0400

    code that returns reference to external html file using relative path

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const http = require('http');
+const fs = require('fs');//require fs module to read file
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<p>hello there!</p>');
-    res.write('<p>hi again!</p>');
-    //creates 2 paragraphs using html. needed to change to 'text/html' instead of 'text/plain'
-    res.end();
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
