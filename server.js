a67c31bb0b4964ac2a672ee82e76ea550f786ae4
Terri Isidro <terri.isidro@gmail.com>
2021-09-11 14:48:15 -0400

    setting header content type using html instead of text/plain and creating two statements w/ paragraph breaks (html)

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => { console.log('request made') });
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<p>hello there!</p>');
+    res.write('<p>hi again!</p>');
+    //creates 2 paragraphs using html. needed to change to 'text/html' instead of 'text/plain'
+    res.end();
+});
 
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000')
-})
+});
