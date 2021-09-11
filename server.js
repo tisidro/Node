04dddc43952ddb9024426238a966f1859f0f4a5c
Terri Isidro <terri.isidro@gmail.com>
2021-09-11 16:02:50 -0400

    code using basic routing between index.html, about.html, and a 404 page

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     res.setHeader('Content-Type', 'text/html');
+    let path = './views/';
 
-    fs.readFile('./views/index.html', (err, data) => {
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+
+        case '/about':
+            path += 'about.html';
+            break;
+
+        default:
+            path += '404.html';
+            break;
+    }
+
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
