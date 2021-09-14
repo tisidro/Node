4766db8c2a1ba25695e21b4520fde4866b945092
Terri Isidro <terri.isidro@gmail.com>
2021-09-14 13:14:36 -0400

    Adding routing to html page rather than the simple get request w/ html text

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>The Home Page</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
-    res.send('<p>About Page</p>');
+    res.sendFile('./views/about.html', { root: __dirname });
 });
