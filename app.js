ae4cb7bf35eb4ba986215e3f197bffbc82213517
Terri Isidro <terri.isidro@gmail.com>
2021-09-23 12:04:33 -0400

    Code passing dynamic data from handlers, filtering through blogs and posting title and nippet as long as blogs.length > 0, also used blogs.forEach to cycle through existing blogs

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    const blogs = [
+        { title: 'First test blog', snippet: 'lorem ipsum dolor sit amet consectetur' },
+        { title: 'Second test blog', snippet: 'lorem ipsum dolor sit amet consectetur' },
+        { title: 'Third test blog', snippet: 'lorem ipsum dolor sit amet consectetur' },
+    ];
+    res.render('index', { title: 'Home', blogs: blogs });//can also have just blogs here instead of blogs: blogs since they are the same!
+});
+
+app.get('/', (req, res) => {
     res.render('index', { title: 'Home' });
 });
 
