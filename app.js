e4b771f99a7f7e9ea7e94b805a185e7be3477fd1
Terri Isidro <terri.isidro@gmail.com>
2021-09-23 11:19:48 -0400

    added dynamic browser tab titles to each page using ejs tags

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create A New Blog' });
 });
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404'), { title: '404' };
 });
